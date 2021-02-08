2b579ec14e71cb83505f8800443808111be9a8b7
Nick <handnf@gmail.com>
2021-02-08 18:56:35 -0500

    Update CI process to copy more content over

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,7 +1,11 @@
+# Copy over from serve.sh
+cp -r ./backend/views ./dist/views
+cp -r ./public ./dist/public
 # Copy compiled components
 cp build/default/frontend/*.js public/src/
 # Host dependencies
 cp -r build/default/node_modules public/
 cp app.yaml dist/app.yaml
 cp package.json dist/package.json
+# cp backend/key.json dist/key.json
 echo "OK"
