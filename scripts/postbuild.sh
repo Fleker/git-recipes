b7ed2f0a8e6125528fa604847c7df8d7f35e37a1
Nick <handnf@gmail.com>
2021-02-08 21:02:59 -0500

    Further logging

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -6,11 +6,14 @@ cp build/default/frontend/*.js public/src/
 ls -R build
 # Host dependencies
 cp -r build/default/node_modules public/node_modules
-ls public
+ls -R dist/
 cp -r ./public ./dist/public
 
 cp app.yaml dist/app.yaml
 cp package.json dist/package.json
 cp .gcloudignore dist/.gcloudignore
+
+ls -R dist/
+
 # cp backend/key.json dist/key.json
 echo "OK"
